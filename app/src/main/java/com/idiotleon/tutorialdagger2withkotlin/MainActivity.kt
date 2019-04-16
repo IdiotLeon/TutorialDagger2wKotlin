0392b2c9c9d9a481aeab17f0103ff83a03f9f970
Yang Lu (Leon) <yanglyu.leon.7@gmail.com>
2019-04-16 16:55:56 -0700

    tutorial: Android Dependency Injection with Dagger2 and Kotlin'

---
+++ app/src/main/java/com/idiotleon/tutorialdagger2withkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.idiotleon.tutorialdagger2withkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
