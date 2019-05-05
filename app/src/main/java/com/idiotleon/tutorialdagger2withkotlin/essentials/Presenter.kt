1fa998d3c999809ab1e42ef4ce84ed7ceff3a313
Yang Lu (Leon) <yanglyu.leon.7@gmail.com>
2019-05-05 12:41:19 -0700

    demo: Demoing the method injection

--- app/src/main/java/com/idiotleon/tutorialdagger2withkotlin/essentials/Presenter.kt
+++ app/src/main/java/com/idiotleon/tutorialdagger2withkotlin/essentials/Presenter.kt
@@ -2,18 +2,21 @@ package com.idiotleon.tutorialdagger2withkotlin.essentials
 
 import com.idiotleon.tutorialdagger2withkotlin.MainActivity
 import dagger.Component
-import dagger.Lazy
 import javax.inject.Inject
 
-class Presenter @Inject constructor(
-    private val client: Lazy<Client>,
-    private val persist: DAO
-) {
+class Presenter {
+    @Inject
+    constructor()
+
+    // An example of method injection.
+    // Method injection is seldom useful.
+    // Use only as a last resort.
+    var client: Client? = null
+        @Inject set
 
     fun connect(show: (String?) -> Unit) {
-        val data = client.get().fetchData()
+        val data = client?.fetchData()
         show(data)
-        persist.store(data)
     }
 }
 
