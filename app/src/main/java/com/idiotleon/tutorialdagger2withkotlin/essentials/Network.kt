7f1c2c5642e46b1894e3d28a41ceaecf2c801d36
Yang Lu (Leon) <yanglyu.leon.7@gmail.com>
2019-05-05 20:19:03 -0700

    tutorial: demoing Lazy and Provider injection

--- app/src/main/java/com/idiotleon/tutorialdagger2withkotlin/essentials/Network.kt
+++ app/src/main/java/com/idiotleon/tutorialdagger2withkotlin/essentials/Network.kt
@@ -6,6 +6,10 @@ import com.idiotleon.tutorialdagger2withkotlin.extra.PROD_ENDPOINT
 import javax.inject.Inject
 import javax.inject.Provider
 
+/*
+ * A Provider<T> is a factory for objects of type T. If your code depends on multiple objects
+ * of type T, inject the Provider<T> and use its get() method to get the instances
+ */
 class Client @Inject constructor(private val connectionFactory: Provider<Connection>) {
     fun fetchData(): String = connectionFactory.get().doReq()
 }
--- app/src/main/java/com/idiotleon/tutorialdagger2withkotlin/modules/provides/Presenter.kt
+++ app/src/main/java/com/idiotleon/tutorialdagger2withkotlin/modules/provides/Presenter.kt
@@ -12,7 +12,16 @@ import javax.inject.Inject
 import javax.inject.Provider
 
 class Presenter @Inject constructor(
+    /*
+     * A Lazy<T> is a factory for objects of type T.
+     * If your code needs a lazily created singleton of type T,
+     * inject the Lazy<T> and use its get() method to get the instances
+     */
     private val client: Lazy<Client>,
+    /*
+     * A Provider<T> is a factory for objects of type T. If your code depends on multiple objects
+     * of type T, inject the Provider<T> and use its get() method to get the instances
+     */
     private val transactionFactory: Provider<Transaction>,
     private val persist: DAO
 ) {
