732c353a07dda1566468e8719a08713105deed59
Yang Lu (Leon) <yanglyu.leon.7@gmail.com>
2019-05-06 09:29:29 -0700

    tutorial: demoing @Named

--- app/src/main/java/com/idiotleon/tutorialdagger2withkotlin/modules/named/Network.kt
+++ app/src/main/java/com/idiotleon/tutorialdagger2withkotlin/modules/named/Network.kt
@@ -13,7 +13,7 @@ class Client @Inject constructor(private val connection: Connection) {
     fun fetchData(): String = connection.doReq()
 }
 
-class Connection @Inject constructor(/*@Named("endpoint")*/ val endpoint: String) {
+class Connection @Inject constructor(@Named("endpoint") val endpoint: String) {
     fun doReq() = endpoint
 }
 
@@ -22,14 +22,26 @@ interface PresenterFactory {
     fun client(): Client
 }
 
+/**
+ * Use @Named to distinguish among different objects with the same type
+ *
+ * Annotate the object: @Provides or @Inject annotation
+ *
+ * Use @Named annotation with the corresponding name parameter to identify a particular object
+ */
 @Module
 object ConnectionModule {
     @JvmStatic
     @Provides
-    // @Named("endpoint")
+    @Named("endpoint")
     fun providesEndpoint() = if (BuildConfig.DEBUG) {
         DEBUG_ENDPOINT
     } else {
         PROD_ENDPOINT
     }
+
+    @JvmStatic
+    @Provides
+    @Named("anotherstring")
+    fun provideAnotherString() = "another string"
 }
